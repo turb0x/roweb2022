1e9c72c07921d238b966e436d005fa1026e4d059
turb0x <pirvugeorgerazvan@gmail.com>
2022-07-11 19:06:19 +0300

    get, update, delete implementation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -18,9 +18,15 @@ class CategoryController extends ApiController
      */
     public function getAll(Request $request)
     {
+        try {
+            $categories = Category::all();
+            return $this->sendResponse($categories->toArray());
+        } catch (\Exception $exception) {
+            Log::error($exception);
 
+            return $this->sendError('Something went wrong, please contact administrator!');
+        }
     }
-
     /**
      * @param Request $request
      * @return JsonResponse
@@ -37,11 +43,8 @@ class CategoryController extends ApiController
                 return $this->sendError('Bad request!', $validator->messages()->toArray());
             }
 
-            $name = $request->get('name');
-            $parentId = $request->get('parent_id');
-
-            if ($parentId) {
-                $parent = Category::find($parentId);
+            if ($request->get('parent_id')) {
+                $parent = Category::find($request->get('parent_id'));
 
                 if ($parent->parent?->parent) {
                     return $this->sendError('You can\'t add a 3rd level subcategory!');
@@ -49,8 +52,8 @@ class CategoryController extends ApiController
             }
 
             $category = new Category();
-            $category->name = $name;
-            $category->parent_id = $parentId;
+            $category->name = $request->get('name');
+            $category->parent_id = $request->get('parent_id');
             $category->save();
 
             return $this->sendResponse($category->toArray());
@@ -66,7 +69,8 @@ class CategoryController extends ApiController
      */
     public function get($id)
     {
-
+        $find = Category::find($id);
+        return $this->sendResponse($find->toArray());
     }
 
     /**
@@ -75,7 +79,27 @@ class CategoryController extends ApiController
      */
     public function update($id, Request $request)
     {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:50',
+                'parent_id' => 'nullable|exists:categories,id'
+            ]);
 
+            if ($validator->fails()) {
+                return $this->sendError('Bad request!', $validator->messages()->toArray());
+            }
+
+            $category = Category::find($id);
+            $category->name = $request->get('name');
+            $category->parent_id = $request->get('parent_id');
+            $category->updated_at = now();
+            $category->save();
+            return $this->sendResponse($category->toArray());
+        } catch (\Exception $exception) {
+            Log::error($exception);
+
+            return $this->sendError('Something went wrong, please contact administrator!');
+        }
     }
 
     /**
@@ -83,6 +107,14 @@ class CategoryController extends ApiController
      */
     public function delete($id)
     {
+        try {
+            $category = Category::find($id);
+            $category->delete();
+            return ("Succes");
+        } catch (\Exception $exception) {
+            Log::error($exception);
 
+            return $this->sendError('Something went wrong, please contact administrator!');
+        }
     }
 }
